f43c18f542ff5043088e87d5082afb7bb5541865
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-02-27 11:17:19 +0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Prakhar Trivedi on 27/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
